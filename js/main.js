f4bccc4109b70cc1f41086b0ee488992f58349af
Chris Brody <chrisbrody@codifyacademy.com>
2015-07-20 22:54:02 -0400

    Version 1 Complete

---
+++ js/main.js
@@ -0,0 +1,66 @@
+
+function userEnglishInput() {
+    var english = document.getElementById('englishInput').value.toLowerCase();
+    var replaceObj = {
+    	a: '.- / ',
+    	b: '-... / ',
+    	c: '-.-. / ',
+    	d: '-.. / ',
+    	e: '. / ',
+    	f: '..-. / ',
+    	g: '--. / ',
+    	h: '.... / ', 
+    	i: '.. / ', 
+    	j: '.--- / ', 
+    	k: '-.- / ', 
+    	l: '.-.. / ', 
+    	m: '-- / ', 
+    	n: '-. / ', 
+    	o: '--- / ', 
+    	p: '.--. / ', 
+    	q: '--.- / ',
+    	r: '.-. / ', 
+    	s: '... / ', 
+    	t: '- / ', 
+    	u: '..- / ', 
+    	v: '...- / ', 
+    	w: '.-- / ', 
+    	x: '-..- / ', 
+    	y: '-.-- / ', 
+    	z: '--.. / ',
+    	'0': '----- / ',
+    	'1': '.---- / ',
+    	'2': '..-- / ',
+    	'3': '...-- / ',
+    	'4': '....- / ',
+    	'5': '----- / ',
+    	'6': '-.... / ',
+    	'7': '--.... / ',
+    	'8': '---.. / ',
+    	'9': '----. / ',
+    	'.': '.-.-.- / ',
+    	',': '--..-- / ',
+    	':': '---... / ',
+    	"'": '.----. / ',
+    	'"': '.----. / ',
+    	'-': '-....- / ',
+    	'@': '.--.-. / ',
+    	'=': '-...- / ',
+    	' ': "",
+    	'?': '..--.. / ',
+    	'\/': '-..-. / '
+
+
+    }
+    english2 = english.replace(/a|b|c|d|e|f|g|h|i|j|k|l|m|n|o|p|q|r|s|t|u|v|w|x|y|z|0|1|2|3|4|5|6|7|8|9|@|.|,|:|"|'|=|\s+|[?=]|\//gi, function(matched) {
+    	
+    	if (replaceObj[matched] == undefined) {
+    		this.splice();
+    	} 
+    	return replaceObj[matched];
+    });
+    document.getElementById('morse').innerHTML = english2;
+    document.getElementById('english').innerHTML = english;
+    console.log(english2.trim());
+    
+}
