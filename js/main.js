81ba43345c7cbd6c3521fe61e4a6c8d52c3edefd
Chris Brody <chrisbrody@codifyacademy.com>
2015-07-22 12:46:23 -0400

    Added Translate and Reset Buttons

--- js/main.js
+++ js/main.js
@@ -1,10 +1,10 @@
 'use strict';
 
-var english, english2;
+var english, morse;
 
 function translateToMorse() {	
 	// print to morse div to display
-	document.getElementById('morse').innerHTML = english2;
+	document.getElementById('morse').innerHTML = morse;
 		
 	// show div if there is more then 1 letter translated
     if (english.length === 0) {
@@ -12,13 +12,13 @@ function translateToMorse() {
     	document.getElementById('morse').innerHTML = 'Nothing to Translate';
     } else{
     	document.getElementById('morseWrapper').style.display="block";
-    };
+    }
 }
 function userEnglishInput() {
 	// get user value and make all lower case
     english = document.getElementById('englishInput').value.toLowerCase();
-    english2 = english.split("");
-    console.log(english2);
+    morse = english.split("");
+    console.log(morse);
     // replace object
     var replaceObj = {
     	a: '.- / ',
@@ -70,13 +70,13 @@ function userEnglishInput() {
     	'\/': '-..-. / '
     }
     // replace characters and retrun
-    english2 = english.replace(/a|b|c|d|e|f|g|h|i|j|k|l|m|n|o|p|q|r|s|t|u|v|w|x|y|z|0|1|2|3|4|5|6|7|8|9|@|.|,|:|"|'|=|\s+|[?=]|\//gi, function(matched) {   	
+    morse = english.replace(/a|b|c|d|e|f|g|h|i|j|k|l|m|n|o|p|q|r|s|t|u|v|w|x|y|z|0|1|2|3|4|5|6|7|8|9|@|.|,|:|"|'|=|\s+|[?=]|\//gi, function(matched) {   	
     	return replaceObj[matched];
     });    
 }
 function resetTranslate() {
 	english = [];
-	english2 = [];
+	morse = [];
 	document.getElementById('englishInput').value = ' ';
 	document.getElementById('morseWrapper').style.display="none";
 	document.getElementById('englishInput').focus();
